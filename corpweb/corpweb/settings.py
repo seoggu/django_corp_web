88caefe22f1f09bb7f77e55184e686e765848a7e
seoggu <pshn123@naver.com>
2023-04-14 16:05:18 +0900

    asd

--- corpweb/corpweb/settings.py
+++ corpweb/corpweb/settings.py
@@ -88,10 +88,21 @@ WSGI_APPLICATION = 'corpweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'database-1.cfihvchv8pfd.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+        'NAME': 'test_db1',
+        'USER': 'postgres',
+        'PASSWORD': '1q2w3e4r',
     }
 }
 

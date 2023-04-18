adefeb46b09dcbcabfdc3d762aac0dc4e21a1223
seoggu <pshn123@naver.com>
2023-04-18 21:38:55 +0900

    final

--- corpweb/corpweb/settings.py
+++ corpweb/corpweb/settings.py
@@ -41,9 +41,9 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -101,23 +101,23 @@ WSGI_APPLICATION = 'corpweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': read_secret('PSQL_HOST'),
-#         'PORT': '5432',
-#         'NAME': 'test_db1',
-#         'USER': 'postgres',
-#         'PASSWORD': read_secret('PSQL_PASSWORD'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': read_secret('PSQL_HOST'),
+        'PORT': '5432',
+        'NAME': read_secret('DB_NAME'),
+        'USER': 'postgres',
+        'PASSWORD': read_secret('PSQL_PASSWORD'),
+    }
+}
 
 
 # Password validation
